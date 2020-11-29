b7e48f8052336333c1fc06e34ac2e19b8dbebfc5
kinoppyd <ppyd@kinoppyd.dev>
2020-11-29 17:06:25 +0900

    Add pre-compile type check settings

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -3,3 +3,16 @@ const typescript =  require('./loaders/typescript')
 
 environment.loaders.prepend('typescript', typescript)
 module.exports = environment
+
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+const path = require("path");
+
+environment.plugins.append(
+    "ForkTsCheckerWebpackPlugin",
+    new ForkTsCheckerWebpackPlugin({
+          typescript: {
+                  configFile: path.resolve(__dirname, "../../tsconfig.json"),
+                },
+          async: false,
+        })
+);
