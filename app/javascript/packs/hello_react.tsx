ccef21cd1ccfac8fc13722a341933377620672c1
kinoppyd <ppyd@kinoppyd.dev>
2020-11-29 17:07:06 +0900

    Rename jsx to tsx

--- app/javascript/packs/hello_react.tsx
+++ app/javascript/packs/hello_react.tsx
@@ -4,19 +4,10 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
 
-const Hello = props => (
+const Hello: React.FC<{ name: string }> = props => (
   <div>Hello {props.name}!</div>
 )
-
-Hello.defaultProps = {
-  name: 'David'
-}
-
-Hello.propTypes = {
-  name: PropTypes.string
-}
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
