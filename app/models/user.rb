891b186de0c02c54e21fe1aec7360e3359a9415d
kinoppyd <WhoIsDissolvedGirl+github@gmail.com>
2020-12-23 03:16:44 +0900

    Create User from oauth hash

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,17 @@
 class User < ApplicationRecord
+  class << self
+    def find_or_create_from_auth_hash(auth_hash)
+      find_or_create_by(auth_hash_to_entity(auth_hash))
+    end
+
+    private
+
+    def auth_hash_to_entity(auth_hash)
+      {
+        display_name: auth_hash["extra"]["id_info"]["name"],
+        email: auth_hash["extra"]["id_info"]["email"],
+        avatar: auth_hash["extra"]["id_info"]["picture"]
+      }
+    end
+  end
 end
