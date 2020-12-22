eca716bac36e4bfeefc549cf18dc7d65809cea56
kinoppyd <WhoIsDissolvedGirl+github@gmail.com>
2020-12-23 03:00:19 +0900

    Add new post form for index

--- client/src/components/templates/Posts/Index.tsx
+++ client/src/components/templates/Posts/Index.tsx
@@ -1,15 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
+import { Input, PrimaryButton, Textarea } from 'smarthr-ui'
 
 import Post, { PostProps } from '../../organisms/Post'
+import { createPost } from '../../../../api/posts'
 
 export interface PostsTemplateProps {
   posts: PostProps[]
 }
 
 const PostsTemplate: React.FC<PostsTemplateProps> = ({posts}) => {
+  const [title, setTitle] = useState("")
+  const [body, setBody] = useState("")
+
+  const handleSubmit = () => {
+    createPost(title, body)
+    .then(() => {document.location.reload()})
+  } 
+
   return(
     <Content>
+      <List>
+        <li>
+          <p>タイトル</p>
+          <Input type="text" onChange={(e) => setTitle(e.target.value)}/>
+        </li>
+        <li>
+          <p>本文</p>
+          <Textarea onChange={(e) => setBody(e.target.value)}/>
+        </li>
+        <li>
+          <PrimaryButton onClick={handleSubmit}>submit</PrimaryButton>
+        </li>
+      </List>
       {posts.map(post => (
         <Post {...post} />
       ))}
@@ -20,6 +43,14 @@ const PostsTemplate: React.FC<PostsTemplateProps> = ({posts}) => {
 const Content = styled.div`
   margin: 32px 10%;
   min-width: 800px;
+`
+const List = styled.ul`
+  padding: 0 24px;
+  list-style: none;
+
+  & > li:not(:first-child) {
+    margin-top: 16px;
+  }
 `
 
 export default PostsTemplate
