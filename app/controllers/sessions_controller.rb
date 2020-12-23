0e6710c46eaab193e94115b2b58908316ff99698
kinoppyd <WhoIsDissolvedGirl+github@gmail.com>
2020-12-23 23:46:37 +0900

    Limit user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,7 @@
 class SessionsController < ApplicationController
 
   def create
+    return render 401 if auth_hash["extra"]["id_info"]["email"] != 'ppyd@kinoppyd.dev'
     @user = User.find_or_create_from_auth_hash(auth_hash)
     session[:email] = @user.email
     redirect_to '/posts'
